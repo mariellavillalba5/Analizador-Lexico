c9cc6d58d6e467d7a385be9c59ee55267b83a259
Mariella <mariellavillalbaom@gmail.com>
2019-01-30 07:42:47 -0600

    Analizador-lexico
    Analizador-lexico

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvTokens = new System.Windows.Forms.DataGridView();
+            this.CODIGO = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.LEXEMA = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.TOKEN = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.POSICION = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnAnalizar = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.btnSalir = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTokens)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvTokens
+            // 
+            this.dgvTokens.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvTokens.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.CODIGO,
+            this.LEXEMA,
+            this.TOKEN,
+            this.POSICION});
+            this.dgvTokens.Location = new System.Drawing.Point(245, 46);
+            this.dgvTokens.Name = "dgvTokens";
+            this.dgvTokens.Size = new System.Drawing.Size(542, 221);
+            this.dgvTokens.TabIndex = 3;
+            this.dgvTokens.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvTokens_CellContentClick);
+            // 
+            // CODIGO
+            // 
+            this.CODIGO.HeaderText = "CODIGO";
+            this.CODIGO.Name = "CODIGO";
+            this.CODIGO.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.CODIGO.Width = 74;
+            // 
+            // LEXEMA
+            // 
+            this.LEXEMA.HeaderText = "LEXEMA";
+            this.LEXEMA.Name = "LEXEMA";
+            this.LEXEMA.Width = 75;
+            // 
+            // TOKEN
+            // 
+            this.TOKEN.HeaderText = "TOKEN";
+            this.TOKEN.Name = "TOKEN";
+            // 
+            // POSICION
+            // 
+            this.POSICION.HeaderText = "POSICION";
+            this.POSICION.Name = "POSICION";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(242, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Tabla de Tokens";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 26);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(115, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Sentencias de Entrada";
+            // 
+            // btnAnalizar
+            // 
+            this.btnAnalizar.Location = new System.Drawing.Point(312, 273);
+            this.btnAnalizar.Name = "btnAnalizar";
+            this.btnAnalizar.Size = new System.Drawing.Size(137, 47);
+            this.btnAnalizar.TabIndex = 8;
+            this.btnAnalizar.Text = "Analizar";
+            this.btnAnalizar.UseVisualStyleBackColor = true;
+            this.btnAnalizar.Click += new System.EventHandler(this.btnAnalizar_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 46);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(227, 221);
+            this.richTextBox1.TabIndex = 9;
+            this.richTextBox1.Text = "";
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(502, 273);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(131, 47);
+            this.btnSalir.TabIndex = 10;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(795, 347);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btnAnalizar);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dgvTokens);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTokens)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DataGridView dgvTokens;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CODIGO;
+        private System.Windows.Forms.DataGridViewTextBoxColumn LEXEMA;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TOKEN;
+        private System.Windows.Forms.DataGridViewTextBoxColumn POSICION;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnAnalizar;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button btnSalir;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        static int x;
+        private void btnAnalizar_Click(object sender, EventArgs e)
+        {
+            //verificamos si la tabla de simbolos tiene mas de un dato
+            if (dgvTokens.RowCount >= 1)
+            {
+                dgvTokens.RowCount = 1;
+            }
+            AnalizadorLexico();  //llamamos a un procedimiento analizar
+        }
+        private void AnalizadorLexico()
+        {   //declaramos un arreglo de caracteres donde vamos almacenar nuestro texto fuente para poder recorrer con mas facilidad
+            Char[] cadena = richTextBox1.Text.ToCharArray();
+            int i=0;  //declarar una variable que nos recorra toda la cadena
+            string conca = ""; //declaramos una variable para poder concatenar
+            while (i < cadena.Length)  //mientras i menor longitud de la cadena
+            {
+                //verificar que no sea salto de linea o espacio y un tabulador
+                if (cadena[i] == ' ' || cadena[i] == '\n' || cadena[i] == '\t')
+                {
+                    i = i + 1;   //avanzamos al siguiente caracter
+                }
+
+                else if (char.IsLetter(cadena[i]))    //si es letra
+                {
+                    x = i; // almacenamos la posicion donde empieza
+                    conca = conca + cadena[i];  //concatenamos ese caracter
+                    i = i + 1;    //avanzamos al sgte caracter
+                    if (i < cadena.Length - 1)  //verificamos que no sea el ultimo caracter
+                    {
+                        //mientras sea numero o letra o guion bajo (_)
+                        while (char.IsLetterOrDigit(cadena[i]) || cadena[i] == '_')
+                        {
+                            conca = conca + cadena[i]; //concatenar y recorrer el sgte. caracter
+                            i = i + 1;
+                            if (i == cadena.Length)  //verificar si llegamos al ultimo caracter
+                            {
+                                break; // si es asi salir del while
+                            }
+                        }
+                        //determinamos si es una palabra reservada o un identificador llamaremos a nuestro metodo PR
+                    }
+                    PR(conca);  // mandamos como parametro la palabra concatenada
+                    conca = ""; //iniciamos lo que concatenamos
+                }
+                //**************
+                else if (char.IsDigit(cadena[i]))    //si es numero
+                {
+                    x = i; // almacenamos la posicion donde empieza
+                    conca = conca + cadena[i];  //concatenamos ese caracter
+                    i = i + 1;    //avanzamos al sgte caracter
+                    if (i < cadena.Length - 1)  //verificamos que no sea el ultimo caracter
+                    {
+                        //mientras sea numero o punto (.)
+                        while (char.IsDigit(cadena[i]) || cadena[i] == '.')
+                        {
+                            conca = conca + cadena[i]; //concatenar y recorrer el sgte. caracter
+                            i = i + 1;
+                            if (i == cadena.Length)  //verificar si llegamos al ultimo caracter
+                            {
+                                break; // si es asi salir del while
+                            }
+                        }
+                    }
+                    PR(conca);  // mandamos como parametro la palabra concatenada
+                    conca = ""; //iniciamos lo que concatenamos
+                }
+                //**************   si son caracteres especiales
+                else if (cadena[i] == '(' || 
+                         cadena[i] == ')' || 
+                         cadena[i] == '[' || 
+                         cadena[i] == ']' || 
+                         cadena[i] == '{' || 
+                         cadena[i] == '}' || 
+                         cadena[i] == '+' || 
+                         cadena[i] == '-' || 
+                         cadena[i] == '*' || 
+                         cadena[i] == '/' || 
+                         cadena[i] == '=' || 
+                         cadena[i] == '%' || 
+                         cadena[i] == '&' || 
+                         cadena[i] == '|' || 
+                         cadena[i] == ',' || 
+                         cadena[i] == ';' || 
+                         cadena[i] == '<' || 
+                         cadena[i] == '>')
+                {
+                    x = i;
+                    conca = conca + cadena[i]; //concatenar y recorrer el sgte. caracter
+                    i = i + 1;
+                    PR(conca);  // mandamos como parametro la palabra concatenada
+                    conca = ""; //iniciamos lo que concatenamos
+                }
+            }
+        }
+        private void PR(string conca)
+        {
+            switch (conca)
+            {  //usaremos caso para saber si es una palabra reservada
+                case "procedimiento": insertar("1", conca, "PAL_RES-PR", x); break;
+                case "funcion": insertar("2", conca, "PAL_RES-PR", x); break;
+                case "principal": insertar("3", conca, "PAL_RES-PR", x); break;
+                case "entero": insertar("4", conca, "PAL_RES-PR", x); break;
+                case "decimal": insertar("5", conca, "PAL_RES-PR", x); break;
+                case "doble": insertar("6", conca, "PAL_RES-PR", x); break;
+                case "caracter": insertar("7", conca, "PAL_RES-PR", x); break;
+                case "cadena": insertar("8", conca, "PAL_RES-PR", x); break;
+                case "booleano": insertar("9", conca, "PAL_RES-PR", x); break;
+                case "si": insertar("10", conca, "PAL_RES-PR", x); break;
+                case "entonces": insertar("11", conca, "PAL_RES-PR", x); break;
+                case "sino": insertar("12", conca, "PAL_RES-PR", x); break;
+                case "para": insertar("13", conca, "PAL_RES-PR", x); break;
+                case "mientras": insertar("14", conca, "PAL_RES-PR", x); break;
+                case "hacer": insertar("15", conca, "PAL_RES-PR", x); break;
+
+                case "(": insertar("16", conca, "PAR_A", x); break;
+                case ")": insertar("17", conca, "PAR_C", x); break;
+                case "[": insertar("18", conca, "COR_A", x); break;
+                case "]": insertar("19", conca, "COR_C", x); break;
+                case "{": insertar("20", conca, "LLAVE_A", x); break;
+                case "}": insertar("21", conca, "LLAVE_C", x); break;
+
+                case "+": insertar("22", conca, "OPER_S", x); break;
+                case "-": insertar("23", conca, "OPER_R", x); break;
+                case "*": insertar("24", conca, "OPER_M", x); break;
+                case "/": insertar("25", conca, "OPER_D", x); break;
+
+                case "=": insertar("26", conca, "OPER_AS", x); break;
+                case "%": insertar("27", conca, "OPER_RES", x); break;
+
+                case "&": insertar("28", conca, "OPER_AND", x); break;
+                case "|": insertar("29", conca, "OPER_OR", x); break;
+
+                case ",": insertar("30", conca, "DEL_SEP", x); break;
+                case ";": insertar("31", conca, "DEL_PYC", x); break;
+
+                case ">": insertar("32", conca, "OPER-COM_MAY", x); break;
+                case "<": insertar("33", conca, "OPER-COM_MEN", x); break;
+
+                default: insertar("59", conca, "variable o identificador-ID", x); break;
+            }
+        }
+        private void insertar(string codigo, string simbolo, string token, int posicion)
+        {   //agregamos una fila a nuestra tabla de simbolos
+            DataGridViewRow row = (DataGridViewRow) dgvTokens.Rows[0].Clone();
+            row.Cells[0].Value = codigo;
+            row.Cells[1].Value = simbolo;
+            row.Cells[2].Value = token;
+            row.Cells[3].Value = posicion;
+
+            dgvTokens.Rows.Add(row);
+        }
+        private void dgvTokens_CellContentClick(object sender, DataGridViewCellEventArgs e){}
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
